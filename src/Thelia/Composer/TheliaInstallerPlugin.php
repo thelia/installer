dca473563e05011c7aea3aaebc6f154fef4187fe
Vincent Lopes-Vicente <vlopesvicente@gmail.com>
2020-10-26 11:32:18 +0100

    Add missing method

--- src/Thelia/Composer/TheliaInstallerPlugin.php
+++ src/Thelia/Composer/TheliaInstallerPlugin.php
@@ -27,4 +27,12 @@ class TheliaInstallerPlugin implements PluginInterface
         $installer = new TheliaInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
+    
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+    
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
 }
